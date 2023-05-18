8be9e043f84c674bf8034fa426758dc27a72f992
ralphvw <114740626+ralphvw@users.noreply.github.com>
2023-05-18 12:28:00 +0000

    Init

---
+++ anagrams.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) < 2:
+    print('Please enter a word')
+    sys.exit()
+
+res = []
+
+with open('words_alpha copy 2.txt', 'r') as f:
+    for word in f.readlines():
+        if sorted(word.strip()) == sorted(sys.argv[1]) and word.strip() != sys.argv[1]:
+            res.append(word.strip())
+
+print(res)            
